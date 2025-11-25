6a390ed439d4271fd6d91b0c321e6cc976eafadb
IVO SCANIO <ivo.e@hotmail.es>
2025-11-24 21:35:12 -0300

    publicrouter

--- src/routers/PublicRouter.jsx
+++ src/routers/PublicRouter.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { Landing } from "../Pages/landing";
 import { LoginPage } from "../Pages/LoginPage";
 import { RegistroPage } from "../Pages/RegistroPage";
 import { Bienvenida } from "../Pages/Bienvenida";
@@ -8,7 +7,6 @@ export const PublicRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Bienvenida />} />
-      {/* <Route path="/inicio" element={<Landing />} /> */}
       <Route path="/login" element={<LoginPage />} />
       <Route path="/registro" element={<RegistroPage />} />
       <Route path="*" element={<Navigate to="/" />} />
