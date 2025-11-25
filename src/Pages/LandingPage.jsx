36b99ddacf0bb6132fae6f385450056afb4de317
IVO SCANIO <ivo.e@hotmail.es>
2025-11-24 21:52:15 -0300

    rename NavBar

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -3,7 +3,7 @@ import { Footer } from "../components/footer/Footer";
 import { Hero } from "../components/hero/Hero";
 import { VideoHero } from "../components/hero/VideoHero";
 import { LogoCarousel } from "../components/logoCarousel/LogoCarousel";
-import { Navbar } from "../components/navBar/NavBar";
+import { Navbar } from "../components/navBar/Navbar";
 import { Servicios } from "../components/Servicios/Servicios";
 
 export const Landing = () => {
